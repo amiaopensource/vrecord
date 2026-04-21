f6591fef3f21359d06b3944c96913dff27d20e62
Andrew Weaver <theandrewjw@gmail.com>
2026-04-21 15:15:18 -0700

    add dependency helper script

---
+++ Release/linux-dependencies.sh
@@ -0,0 +1,14 @@
+sudo apt-get install -y --no-install-recommends \
+libgtk2.0-dev \
+libgtk-3-dev \
+bison \
+flex \
+texinfo \
+git \
+mkvtoolnix \
+gnuplot \
+xmlstarlet \
+libiec61883-dev \
+libraw1394-dev \
+libavc1394-dev \
+libavc1394-tools
